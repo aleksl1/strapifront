4e3009f5085e6f9f26ec7fe0a740b634a0ea5227
aleksslawek <aleksander.slawek@mindconsulting.pl>
2022-09-17 19:32:22 +0200

    Navbar position fixed Add temp pages Style contact section

--- pages/kancelaria/index.tsx
+++ pages/kancelaria/index.tsx
@@ -1,9 +1,19 @@
 import { FunctionComponent } from "react";
+import AboutSection from "../../components/HomePageComponents/AboutSection";
 
 interface AboutPageProps {}
 
 const AboutPage: FunctionComponent<AboutPageProps> = () => {
-  return <div></div>;
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <AboutSection />
+    </div>
+  );
 };
 
 export default AboutPage;
--- pages/uslugi/index.tsx
+++ pages/uslugi/index.tsx
@@ -1,9 +1,16 @@
 import { FunctionComponent } from "react";
+import OfferSection from "../../components/HomePageComponents/OfferSection";
 
 interface OffersPageProps {}
 
 const OffersPage: FunctionComponent<OffersPageProps> = () => {
-  return <div></div>;
+  return (
+    <div>
+      <br /> <br />
+      <br /> <br />
+      <OfferSection />
+    </div>
+  );
 };
 
 export default OffersPage;
