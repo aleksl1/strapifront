cb19e1684bdc28b73061785d5e461d1d5b85f318
aleksslawek <aleksander.slawek@mindconsulting.pl>
2022-09-18 15:04:47 +0200

    Correct date helper

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -2,5 +2,8 @@ import { ParseDateType } from "../models/Other.models";
 
 export const parseDate: ParseDateType = (dbDate: string) => {
   const date = new Date(Date.parse(dbDate));
-  return `${date.toLocaleDateString()} o ${date.getHours()}:${date.getMinutes()}`;
+  if (date.getMinutes() >= 10) {
+    return `${date.toLocaleDateString()} o ${date.getHours()}:${date.getMinutes()}`;
+  } else
+    return `${date.toLocaleDateString()} o ${date.getHours()}:0${date.getMinutes()}`;
 };
