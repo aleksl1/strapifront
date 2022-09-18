4d1e632e65804315a04bf9257e008499380ee9d3
aleksslawek <aleksander.slawek@mindconsulting.pl>
2022-09-18 15:05:58 +0200

    Add const to helper

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -2,8 +2,8 @@ import { ParseDateType } from "../models/Other.models";
 
 export const parseDate: ParseDateType = (dbDate: string) => {
   const date = new Date(Date.parse(dbDate));
-  if (date.getMinutes() >= 10) {
-    return `${date.toLocaleDateString()} o ${date.getHours()}:${date.getMinutes()}`;
-  } else
-    return `${date.toLocaleDateString()} o ${date.getHours()}:0${date.getMinutes()}`;
+  const minutes = date.getMinutes();
+  if (minutes >= 10) {
+    return `${date.toLocaleDateString()} o ${date.getHours()}:${minutes}`;
+  } else return `${date.toLocaleDateString()} o ${date.getHours()}:0${minutes}`;
 };
