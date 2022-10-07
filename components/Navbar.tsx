e9436758f4ec994bb7c435d25ae9ab1864a88804
aleksslawek <aleksander.slawek@mindconsulting.pl>
2022-10-07 22:42:39 +0200

    Add navbar for desktop Style navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { FunctionComponent, useState } from "react";
 import styles from "../styles/Navbar.module.css";
 
 interface NavbarProps {}
 
+const navRoutes = [
+  { name: "Strona Główna", path: "/" },
+  { name: "Obwieszczenia", path: "/obwieszczenia" },
+  { name: "O Kancelarii", path: "/kancelaria" },
+  { name: "Usługi", path: "/usługi" },
+  { name: "Kontakt", path: "/#contact" },
+];
+
 const Navbar: FunctionComponent<NavbarProps> = () => {
   const [isChecked, setIsChecked] = useState(false);
+  const router = useRouter();
   const toggleNav = () => {
     setIsChecked(!isChecked);
   };
@@ -14,7 +24,11 @@ const Navbar: FunctionComponent<NavbarProps> = () => {
       <nav>
         <div className={styles.navbar}>
           <div className={`${styles.container} ${styles["nav-container"]}`}>
-            <div className={styles["hamburger-lines"]} onClick={toggleNav}>
+            <div
+              className={styles["hamburger-lines"]}
+              onClick={toggleNav}
+              id={styles.mobile}
+            >
               <span
                 className={
                   !isChecked
@@ -37,7 +51,7 @@ const Navbar: FunctionComponent<NavbarProps> = () => {
                 }
               ></span>
             </div>
-            <div className={styles.logo}>
+            <div className={styles.logo} onClick={() => router.push("/")}>
               <h1>INLEX</h1>
               <h2>Kancelaria Prawnicza</h2>
             </div>
@@ -47,22 +61,34 @@ const Navbar: FunctionComponent<NavbarProps> = () => {
                   ? styles["menu-items"]
                   : `${styles["menu-items"]} ${styles["menu-items-open"]}`
               }
+              id={styles["mobile-items"]}
             >
-              <li onClick={toggleNav}>
-                <Link href="/">Strona Główna</Link>
-              </li>
-              <li onClick={toggleNav}>
-                <Link href="/obwieszczenia">Obwieszczenia</Link>
-              </li>
-              <li onClick={toggleNav}>
-                <Link href="/kancelaria">O Kancelarii</Link>
-              </li>
-              <li onClick={toggleNav}>
-                <Link href="/uslugi">Usługi</Link>
-              </li>
-              <li onClick={toggleNav}>
-                <Link href="/#contact">Kontakt</Link>
-              </li>
+              {navRoutes.map((navRoute) => {
+                const isActive = router.asPath === navRoute.path;
+                return (
+                  <li
+                    key={navRoute.path}
+                    onClick={toggleNav}
+                    className={isActive ? styles.active : ""}
+                  >
+                    <Link href={navRoute.path}>{navRoute.name}</Link>
+                  </li>
+                );
+              })}
+            </div>
+            <div id={styles["desktop-items"]}>
+              {navRoutes.map((navRoute) => {
+                const isActive = router.asPath === navRoute.path;
+                return (
+                  <li
+                    key={navRoute.path}
+                    onClick={toggleNav}
+                    className={isActive ? styles.active : ""}
+                  >
+                    <Link href={navRoute.path}>{navRoute.name}</Link>
+                  </li>
+                );
+              })}
             </div>
           </div>
         </div>
